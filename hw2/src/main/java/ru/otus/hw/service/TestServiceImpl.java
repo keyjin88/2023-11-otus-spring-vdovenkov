cd28cab9480939acade6e53e84e15c3f62f93c74
AV <keyjin88@gmail.com>
2023-12-07 16:25:17 +0300

    Реализовал логику получения ответов и учета результатов

--- hw2/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw2/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -2,8 +2,11 @@ package ru.otus.hw.service;
 
 import lombok.RequiredArgsConstructor;
 import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Answer;
 import ru.otus.hw.domain.Student;
 import ru.otus.hw.domain.TestResult;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 public class TestServiceImpl implements TestService {
@@ -19,8 +22,21 @@ public class TestServiceImpl implements TestService {
         var questions = questionDao.findAll();
         var testResult = new TestResult(student);
 
-        for (var question: questions) {
-            var isAnswerValid = false; // Задать вопрос, получить ответ
+        for (int i = 0; i < questions.size(); i++) {
+            var question = questions.get(i);
+            var isAnswerValid = false;
+            ioService.printFormattedLine("Question %d: %s", i, question.text());
+            List<Answer> answers = question.answers();
+            for (int j = 0; j < answers.size(); j++) {
+                Answer answer = answers.get(j);
+                ioService.printFormattedLine("%d) %s", j, answer.text());
+            }
+            var answer = ioService.readIntForRangeWithPrompt(
+                    0,
+                    question.answers().size(),
+                    "Enter answer number: ",
+                    "Answer must be from 0 to %d".formatted(question.answers().size() - 1));
+            isAnswerValid = question.answers().get(answer).isCorrect();
             testResult.applyAnswer(question, isAnswerValid);
         }
         return testResult;
