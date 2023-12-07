0391fbcccd10067164cba81d753a78ae1ef463d1
AV <keyjin88@gmail.com>
2023-12-07 16:43:42 +0300

    Добавил тест, выпилил сборку uber-jar, чтоб не сыпала ворнингами

--- hw2/src/main/java/ru/otus/hw/config/ServicesConfig.java
+++ hw2/src/main/java/ru/otus/hw/config/ServicesConfig.java
@@ -4,7 +4,16 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import ru.otus.hw.dao.QuestionDao;
-import ru.otus.hw.service.*;
+import ru.otus.hw.service.IOService;
+import ru.otus.hw.service.ResultService;
+import ru.otus.hw.service.ResultServiceImpl;
+import ru.otus.hw.service.StreamsIOService;
+import ru.otus.hw.service.StudentService;
+import ru.otus.hw.service.StudentServiceImpl;
+import ru.otus.hw.service.TestRunnerService;
+import ru.otus.hw.service.TestRunnerServiceImpl;
+import ru.otus.hw.service.TestService;
+import ru.otus.hw.service.TestServiceImpl;
 
 @Configuration
 public class ServicesConfig {
