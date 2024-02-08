0260e572d80a50da26c4ca2394bdafe0fcf11be9
AV <keyjin88@gmail.com>
2024-02-08 14:16:25 +0300

    fix tests

--- hw6/src/main/java/ru/vavtech/hw6/models/Author.java
+++ hw6/src/main/java/ru/vavtech/hw6/models/Author.java
@@ -7,12 +7,14 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@EqualsAndHashCode
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
